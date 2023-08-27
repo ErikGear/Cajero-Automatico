3fc392b04c94d04b96483ef63337422b20f971e7
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-26 21:32:05 -0600

    Agregando archivo app.js

---
+++ JavaScript/app.js
@@ -0,0 +1,36 @@
+var cuentas = [
+    {
+        nombre:"Mali",
+        saldo: 200,
+        password:"WWdkc,:5]e"
+    },
+    {
+        nombre:"Gera",
+        saldo: 290,
+        password: "=sd>yTr49b"
+    },
+    {
+        nombre:"Mauri",
+        saldo:67,
+        password: "NB!f4t&yVx"
+    },
+    {
+        nombre:"John",
+        saldo: 600,
+        password: "mXE*`?57Hj"
+    },
+    {
+        nombre:"Erik",
+        saldo: 890,
+        password: "M7sm/VM<JY"
+    },
+    {
+        nombre:"Alfredo",
+        saldo: 300,
+        password: "kN.$4uH]P}"
+    }
+];
+
+const usuarios = document.getElementById("usuarios");
+const usuario = usuarios.value;
+
