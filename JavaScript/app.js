6341bb607524896c9de713dca8bfc9ad65d58fa1
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-30 20:48:12 -0600

    Refactorizando codigo

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var cuentas = [
   {
     nombre: "Mali",
@@ -37,6 +35,16 @@ const usuarios = document.getElementById("usuarios");
 const password = document.getElementById("password");
 const boton = document.getElementById("check");
 const form = document.getElementById("form");
+const datos = document.getElementById("datos");
+
+const monto = document.getElementById("monto");
+const saldo = document.getElementById("consultar");
+const ingreso = document.getElementById("ingresar");
+const retiro = document.getElementById("retirar");
+const sesion = document.getElementById("salir");
+const mensaje = document.getElementById("mensaje");
+var validacionUsuario = 0;
+
 
 /*Empleando Modularidad*/
 function busquedaUsuario(usuarios, pwd, usuario) {
@@ -51,20 +59,24 @@ function busquedaUsuario(usuarios, pwd, usuario) {
   return -1;
 }
 
+
 const validacionCredenciales = function (e) {
   e.preventDefault();
   const paragraphWarning = document.getElementById("warning");
   const usuario = usuarios.value;
   const pwd = password.value;
   const validacion = busquedaUsuario(cuentas, pwd, usuario);
+
   let warning = "";
 
   if (validacion >= 0) {
+    validacionUsuario = validacion;
     warning += "Enviado";
     paragraphWarning.textContent = warning;
     paragraphWarning.style.color = "#8ac926";
-    //El siguiente metodo nos permitira cambiar de pagina HTML
-    window.location = "../pages/usuario.html";
+    form.reset();
+
+    datos.style.display = "block";
   } else {
     warning += "Credenciales invalidas";
     paragraphWarning.textContent = warning;
@@ -76,7 +88,37 @@ const validacionCredenciales = function (e) {
 boton.addEventListener("click", validacionCredenciales);
 
 
-//########################################################
-/*Lógica de la pagina usuario.html*/
+function consultarSaldo(){
+  mensaje.style.display= "block";
+  mensaje.textContent = cuentas[validacionUsuario];
+}
+function ingresarSaldo() {
+  if (cuentas[validacionUsuario].saldo >= 990.0) {
+    mensaje.style.display= "block";
+    mensaje.textContent = `dispone de ${cuentas[validacionUsuario].saldo} el cual es el monto maximo permitido`;
+  } else {
+    monto.style.display = "block";
+    cuentas[validacionUsuario].saldo += monto.value;
+    mensaje.style.display= "block";
+    mensaje.textContent = `dispone de ${cuentas[validacionUsuario].saldo}`;
+  }
+}
+function retirarSaldo() {
+  if (cuentas[validacionUsuario].saldo == 10) {
+    mensaje.style.display= "block";
+    mensaje.textContent = `dispone de ${cuentas[validacionUsuario].saldo} el cual es el saldo minimo`;
+  } else {
+    monto.style.display= "block";
+    cuentas[validacionUsuario].saldo -= monto.value;
+    mensaje.style.display= "block";
+    mensaje.textContent = `dispone de ${cuentas[validacionUsuario].saldo}`;
+  }
+}
+function cerrarSesion() {
+  datos.style.display = "none";
+}
 
-
+saldo.addEventListener("click", consultarSaldo);
+ingreso.addEventListener("click", ingresarSaldo);
+retiro.addEventListener("click", retirarSaldo);
+sesion.addEventListener("click", cerrarSesion);
